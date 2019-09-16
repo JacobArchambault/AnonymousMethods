4b647ab1cf884cfeaae9844c33793f5376bcfdec
Jacob Archambault <jacob.archambault@waystar.com>
2019-09-16 09:15:16 -0400

    Complete project

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace AnonymousMethods
 {
@@ -7,12 +10,34 @@ namespace AnonymousMethods
         static void Main(string[] args)
         {
             Console.WriteLine("***** Anonymous Methods *****\n");
+            Car c1 = new Car("SlugBug", 100, 10);
+
+            int aboutToBlowCounter = 0;
 
             // Register event handlers as anonymous methods.
             c1.AboutToBlow += delegate
             {
-                global::System.Console.WriteLine("Eek! Going too fast!");
+                aboutToBlowCounter++;
+                Console.WriteLine("Eek! Going too fast!");
             };
+
+            c1.AboutToBlow += delegate (object sender, CarEventArgs e)
+            {
+                aboutToBlowCounter++;
+                Console.WriteLine("Message from Car: {0}", e.msg);
+            };
+
+            c1.Exploded += delegate (object sender, CarEventArgs e)
+            {
+                Console.WriteLine("Fatal Message from Car: {0}", e.msg);
+            };
+
+            // This will eventually trigger the events.
+            for (int i = 0; i < 6; i++)
+                c1.Accelerate(20);
+
+            Console.WriteLine("AboutToBlow event was fired {0} times.", aboutToBlowCounter);
+            Console.ReadLine();
         }
     }
 }
