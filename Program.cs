fb2a7751263b2fce4150ffd9429db61b2aad56c9
Jacob Archambault <jacob.archambault@waystar.com>
2019-09-16 09:04:52 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AnonymousMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Anonymous Methods *****\n");
+
+            // Register event handlers as anonymous methods.
+            c1.AboutToBlow += delegate
+            {
+                global::System.Console.WriteLine("Eek! Going too fast!");
+            };
+        }
+    }
+}
